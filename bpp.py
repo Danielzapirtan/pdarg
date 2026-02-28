01458df24872782ca3a20b5c77471d3b4a2273c3
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-02-28 20:54:34 +0000

    pdftext

--- bpp.py
+++ bpp.py
@@ -1,4 +1,4 @@
 from pdftext.extraction import dictionary_output
 
 # Get structured data with font information
-structured_data = dictionary_output("/content/drive/MyDrive/input.pdf", sort=True, keep_chars=True)
+structured_data = dictionary_output("/home/daniel/input.pdf", sort=True, keep_chars=True)
