a83d5e2c3f8840bbc9cfbd7a0cb930190262781a
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-02-28 20:52:39 +0000

    m

--- bpp.py
+++ bpp.py
@@ -1,35 +1,4 @@
-from dedoc import DedocManager
-from dedoc.attachments_handler import AttachmentsHandler
-from dedoc.converters import DocxConverter, PNGConverter, ConverterComposition
-from dedoc.metadata_extractors import BaseMetadataExtractor, MetadataExtractorComposition
-from dedoc.readers import DocxReader, PdfAutoReader, PdfImageReader, ReaderComposition
-from dedoc.structure_extractors import DefaultStructureExtractor, StructureExtractorComposition
-from dedoc.structure_constructors import TreeConstructor, StructureConstructorComposition
+from pdftext.extraction import dictionary_output
 
-dedoc_manager = DedocManager(
-    manager_config={
-        "attachments_handler": AttachmentsHandler(),
-        "converter": ConverterComposition([DocxConverter(), PNGConverter()]),
-        "reader": ReaderComposition([DocxReader(), PdfAutoReader(), PdfImageReader()]),
-        "structure_extractor": StructureExtractorComposition(extractors={DefaultStructureExtractor.document_type: DefaultStructureExtractor()}, default_key=DefaultStructureExtractor.document_type),
-        "structure_constructor": StructureConstructorComposition(constructors={"tree": TreeConstructor()}, default_constructor=TreeConstructor()),
-        "document_metadata_extractor": MetadataExtractorComposition([BaseMetadataExtractor()])
-    }
-)
-import os
-import tempfile
-import wget
-
-parsed_documents = []
-
-with tempfile.TemporaryDirectory() as tmpdir:
-  from google.colab import drive
-  drive.mount('/content/drive')
-  document_names = []
-  document_names.append('/content/drive/MyDrive/input.pdf')
-
-  for document_name in document_names:
-    parsed_document = dedoc_manager.parse(file_path=document_name)
-    parsed_documents.append(parsed_document)
-pdf_document = parsed_documents[0]
-print("PDF document:\n", pdf_document.content.structure.subparagraphs[0].text)
+# Get structured data with font information
+structured_data = dictionary_output("document.pdf", sort=True, keep_chars=True)
